a947d407fa69965c0bf0df395cd6fdbcbf5a4477
Alex <alex.akten@outlook.com>
2023-12-25 11:26:54 -0400

    text size mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,28 +11,30 @@ export default function Landing() {
             <h1 className="text-4xl lg:text-8xl">
               Restore the balance of our ecosystem with carbon credits.
             </h1>
-            <div className="gap-4 flex lg:flex-row">
+            <div className="flex gap-4 lg:flex-row">
               <button
-                className="bg-orange text-darkgreen rounded-full px-4 sm:px-8 py-3"
+                className="bg-orange text-darkgreen rounded-full px-4 py-3 sm:px-8"
                 type="button"
               >
                 For landowners →
               </button>
               <button
-                className="bg-lightgreen text-orange rounded-full px-4 sm:px-8 py-3"
+                className="bg-lightgreen text-orange rounded-full px-4 py-3 sm:px-8"
                 type="button"
               >
                 For investors →
               </button>
             </div>
           </div>
-          <div className="text-md flex h-full w-full max-w-lg items-end justify-end lg:text-2xl">
+          <div className="text-lg flex h-full w-full max-w-lg items-end justify-end lg:text-2xl">
             Airbon helps landowners in South America monitor and measure their
             forests to create high quality tech-verified carbon credits.
           </div>
         </div>
 
-        <div className="">
+
+
+        <div>
           <p className="text-orange">Scroll to learn more ↓</p>
         </div>
       </section>
