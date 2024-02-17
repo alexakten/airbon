e275e4514a0a8778019071d0603de68bac94f166
Alex <alex.akten@outlook.com>
2024-02-17 12:27:02 +0100

    clicka på hela kortet

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -20,33 +20,33 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <div className="grid min-h-[400px] w-full grid-rows-2 overflow-hidden rounded-xl border border-black">
-      <div className="relative row-span-1 bg-gray-300">
-        <Image
-          src={project.projectImage}
-          alt={project.projectName}
-          layout="fill"
-          objectFit="cover"
-        ></Image>
-      </div>
-      <div className="row-span-1 flex flex-col p-4">
-        {/* <p className="text-sm opacity-50">{project.projectLocation}</p> */}
-        <h3 className="text-2xl">{project.projectName}</h3>
-        <p className="pt-1 text-base opacity-75">
-          {project.projectDescription.length > 70
-            ? `${project.projectDescription.slice(0, 80)}...`
-            : project.projectDescription}
-        </p>
-        <div className="flex h-full items-end justify-end">
-          <Link href={`/projects/${project.slug}`}>
+    <Link href={`/projects/${project.slug}`}>
+      <div className="grid min-h-[400px] w-full grid-rows-2 overflow-hidden rounded-xl border border-black">
+        <div className="relative row-span-1 bg-gray-300">
+          <Image
+            src={project.projectImage}
+            alt={project.projectName}
+            layout="fill"
+            objectFit="cover"
+          ></Image>
+        </div>
+        <div className="row-span-1 flex flex-col p-4">
+          {/* <p className="text-sm opacity-50">{project.projectLocation}</p> */}
+          <h3 className="text-2xl">{project.projectName}</h3>
+          <p className="pt-1 text-base opacity-75">
+            {project.projectDescription.length > 70
+              ? `${project.projectDescription.slice(0, 100)}...`
+              : project.projectDescription}
+          </p>
+          <div className="flex h-full items-end justify-end">
             <button className="transform transition-transform duration-200 ease-in-out hover:translate-x-1">
               {" "}
               Learn more
               <span className={inter.className}> -&gt;</span>
             </button>
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
