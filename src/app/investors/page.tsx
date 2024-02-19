25182b8753738cdfddfb68127b9b521e014b2fc6
Alex <alex.akten@outlook.com>
2024-02-19 10:31:45 +0100

    farmers page investors page

--- src/app/investors/page.tsx
+++ src/app/investors/page.tsx
@@ -1,15 +1,16 @@
 import Navbar from "../components/Navbar";
+import Link from "next/link";
+import ProjectGrid from "../components/ProjectGrid";
+import { Inter } from "next/font/google";
 
-export default function Landing() {
+const inter = Inter({ subsets: ["latin"] });
+
+export default function InvestorsPage() {
   return (
-    <main className="flex flex-col items-center bg-lightgray px-3 text-base text-offblack sm:px-8 2xl:text-lg">
-      <section
-        className="relative flex max-w-8xl flex-col py-6"
-        style={{ height: "100svh" }}
-      >
-        <Navbar navColor="offblack"></Navbar>
-
-        <div className="flex flex-col gap-4 border-offblack pt-20 lg:flex-row lg:gap-0 lg:border-b lg:pb-12">
+    <main className="relative flex flex-col items-center bg-lightgray px-3 text-base text-offblack sm:px-8 2xl:text-lg">
+      <section className="relative flex h-[100svh] max-w-8xl flex-col items-center sm:h-auto">
+        <Navbar navColor="offblack" />
+        <div className="flex flex-col gap-4 border-offblack pt-32 lg:flex-row lg:gap-0 lg:border-b lg:pb-12">
           <div className="flex max-w-6xl flex-col gap-4 lg:gap-14 2xl:max-w-7xl">
             <div className="flex flex-col gap-3">
               <div className="flex items-center gap-2 px-2 py-2 pl-2">
@@ -20,29 +21,32 @@ export default function Landing() {
                 <p className="text-sm">v.01 launching soon</p>
               </div>
               <h1 className="text-[clamp(32px,5.5vw,84px)] leading-[1.05]">
-                Go net zero and support farmers with carbon credits.
+                Go net zero and support farmers with carbon
               </h1>
             </div>
 
             <div className="flex gap-4 lg:flex-row">
-              <button className="rounded-full border border-offblack px-4 py-3 hover:bg-green-400 hover:text-offblack sm:px-8">
-                Start investing →
-              </button>
+              <Link
+                href="mailto:alex.akten@outlook.com"
+                className="rounded-full border border-offblack px-4 py-3 hover:bg-green-400 hover:text-offblack sm:px-8"
+              >
+                Get started <span className={inter.className}>-&gt;</span>
+              </Link>
             </div>
           </div>
-          <p className="absolute bottom-4 w-full max-w-lg  items-end justify-end text-lg lg:static lg:flex lg:text-2xl">
-            Airbon helps farmers in Brazil adopt carbon farming practices to
-            create high quality tech-verified carbon credits.
+          <p className="absolute bottom-4 flex w-full max-w-lg items-end justify-end pt-4 text-lg sm:static sm:text-2xl">
+            Airbon helps farmers adopt carbon farming practices to create high
+            quality tech-verified carbon credits.
           </p>
         </div>
 
-        {/* Steps on desktop*/}
-        <div className="hidden pt-16 lg:block">
-          <div className="grid grid-cols-4 gap-20">
+        {/* -------------------------Steps on desktop----------------------------- */}
+        <div className="hidden pt-16 sm:block">
+          <div className="grid grid-cols-2 gap-8 lg:grid-cols-4 lg:gap-20">
             <div className="col-span-1">
               <p className="text-sm">1</p>
-              <h2 className="pt-6 text-2xl">Educate</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Enable</h2>
+              <p className="pt-2">
                 We help farmers get started with carbon farming to generate
                 credits.
               </p>
@@ -50,8 +54,8 @@ export default function Landing() {
 
             <div className="col-span-1">
               <p className="text-sm">2</p>
-              <h2 className="pt-6 text-2xl">Measure</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Measure</h2>
+              <p className="pt-2">
                 We measure and verify impact using satellite imagery, IoT
                 devices and AI.
               </p>
@@ -59,8 +63,8 @@ export default function Landing() {
 
             <div className="col-span-1">
               <p className="text-sm">3</p>
-              <h2 className="pt-6 text-2xl">Analyze</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Analyze</h2>
+              <p className="pt-2">
                 Our digital platform allows for analysis and reporting of carbon
                 sequestration.
               </p>
@@ -68,8 +72,8 @@ export default function Landing() {
 
             <div className="col-span-1">
               <p className="text-sm">4</p>
-              <h2 className="pt-6 text-2xl">Sell</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Sell</h2>
+              <p className="pt-2">
                 We connect farmers with climate conscious businesses going
                 net-zero.
               </p>
@@ -78,23 +82,23 @@ export default function Landing() {
         </div>
       </section>
 
-      {/* Steps on mobile*/}
-      <section className="pb-40 lg:hidden lg:pb-0">
-        <div className="pt-24 ">
-          <div className="grid grid-cols-1 gap-12">
+      {/* -------------------------Steps on mobile-------------------------------- */}
+      <section className="w-full py-24 sm:hidden lg:pb-0">
+        <div className="flex justify-start">
+          <div className="grid w-full grid-cols-1 gap-12 sm:grid-cols-2">
             <div className="col-span-1">
               <p className="text-sm">1</p>
-              <h2 className="pt-6 text-2xl">Educate</h2>
-              <p className="pt-3">
-                We help farmers get started with carbon farming practices that
-                generate credits.
+              <h2 className="pt-4 text-2xl">Enable</h2>
+              <p className="pt-2">
+                We help farmers get started with carbon farming to generate
+                credits.
               </p>
             </div>
 
             <div className="col-span-1">
               <p className="text-sm">2</p>
-              <h2 className="pt-6 text-2xl">Measure</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Measure</h2>
+              <p className="pt-2">
                 We measure and verify impact using satellite imagery, IoT
                 devices and AI.
               </p>
@@ -102,8 +106,8 @@ export default function Landing() {
 
             <div className="col-span-1">
               <p className="text-sm">3</p>
-              <h2 className="pt-6 text-2xl">Analyze</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Analyze</h2>
+              <p className="pt-2">
                 Our digital platform allows for analysis and reporting of carbon
                 sequestration.
               </p>
@@ -111,8 +115,8 @@ export default function Landing() {
 
             <div className="col-span-1">
               <p className="text-sm">4</p>
-              <h2 className="pt-6 text-2xl">Sell</h2>
-              <p className="pt-3">
+              <h2 className="pt-4 text-2xl">Sell</h2>
+              <p className="pt-2">
                 We connect farmers with climate conscious businesses going
                 net-zero.
               </p>
@@ -121,19 +125,10 @@ export default function Landing() {
         </div>
       </section>
 
-      {/* <section className="text-offblack bg-deepgreen relative flex flex-col items-center px-2 py-40 sm:px-8 ">
-        <div className="grid h-full w-full grid-cols-2">
-          <div className="col-span-1 flex h-full w-full max-w-lg flex-col items-start justify-center">
-            <h2 className="text-6xl">It&apos;s time to heal our planet.</h2>
-            <p className="pt-6">
-              It&apos;s time to heal our planet with carbon removal. By applying
-              biochar to soils we permanently remove CO2 for 1000+ years from
-              the atmosphere. This supports people and food systems to prosper.
-              A win-win for the people and the planet.
-            </p>
-          </div>
-        </div>
-      </section> */}
+      <section className="w-full max-w-8xl py-12 sm:py-40">
+        <h2 className="pb-8 text-3xl sm:text-4xl">Discover projects</h2>
+        <ProjectGrid cards={8} />
+      </section>
     </main>
   );
 }
