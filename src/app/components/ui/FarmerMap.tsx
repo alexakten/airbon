8fce193d682064c89692f573022fb5564cc9c0b6
Alex <alex.akten@outlook.com>
2024-03-29 10:25:50 +0100

    remove map

--- src/app/components/ui/FarmerMap.tsx
+++ src/app/components/ui/FarmerMap.tsx
@@ -47,49 +47,50 @@ const FarmerMap: React.FC<FarmerMapProps> = ({ farmers }) => {
   }
 
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={3}
-      minZoom={2}
-      scrollWheelZoom={false}
-      style={{ height: "40rem", width: "100%", borderRadius: "1rem" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {farmers.map((farmer) => (
-        <Marker key={farmer.id} position={[farmer.latitude, farmer.longitude]}>
-          <Popup>
-            <div className="flex flex-col p-0">
-              <div className="relative mt-2 h-36 w-56 overflow-hidden rounded-md bg-gray-200">
-                <Image
-                  className="object-cover"
-                  src={farmer.image}
-                  alt={farmer.name}
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="mt-3 flex items-end justify-between">
-                <div className="">
-                  <h3 className="font-semibold">{farmer.name}</h3>
-                  <h5 className="mt-0.5 opacity-50">{farmer.location}</h5>
-                  <h5 className="mt-0.5 opacity-50">{farmer.size} hectares</h5>
-                </div>
-                <div>
-                  <Link href={`/farmers/${farmer.slug}`}>
-                    <button className="rounded-md bg-darkgreen px-2 py-2 text-white hover:bg-deepgreen">
-                      See farm
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div></div>
+    // <MapContainer
+    //   center={[51.505, -0.09]}
+    //   zoom={3}
+    //   minZoom={2}
+    //   scrollWheelZoom={false}
+    //   style={{ height: "40rem", width: "100%", borderRadius: "1rem" }}
+    // >
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    //   {farmers.map((farmer) => (
+    //     <Marker key={farmer.id} position={[farmer.latitude, farmer.longitude]}>
+    //       <Popup>
+    //         <div className="flex flex-col p-0">
+    //           <div className="relative mt-2 h-36 w-56 overflow-hidden rounded-md bg-gray-200">
+    //             <Image
+    //               className="object-cover"
+    //               src={farmer.image}
+    //               alt={farmer.name}
+    //               layout="fill"
+    //               objectFit="cover"
+    //             />
+    //           </div>
+    //           <div className="mt-3 flex items-end justify-between">
+    //             <div className="">
+    //               <h3 className="font-semibold">{farmer.name}</h3>
+    //               <h5 className="mt-0.5 opacity-50">{farmer.location}</h5>
+    //               <h5 className="mt-0.5 opacity-50">{farmer.size} hectares</h5>
+    //             </div>
+    //             <div>
+    //               <Link href={`/farmers/${farmer.slug}`}>
+    //                 <button className="rounded-md bg-darkgreen px-2 py-2 text-white hover:bg-deepgreen">
+    //                   See farm
+    //                 </button>
+    //               </Link>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </Popup>
+    //     </Marker>
+    //   ))}
+    // </MapContainer>
   );
 };
 
