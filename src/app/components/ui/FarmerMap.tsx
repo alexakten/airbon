e3e942f6a8ce65368c1b6e0fa3e866c18e39286a
Alex <alex.akten@outlook.com>
2024-03-29 21:41:30 +0100

    map import

--- src/app/components/ui/FarmerMap.tsx
+++ src/app/components/ui/FarmerMap.tsx
@@ -1,12 +1,26 @@
-"use client";
-import React, { useState, useEffect } from "react";
+"use client"
+import React from "react";
 import dynamic from "next/dynamic";
-
 import Link from "next/link";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Image from "next/image";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+
+// Dynamically import the Map components, ensuring they're only loaded client-side
+const MapContainerNoSSR = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), {
+  ssr: false
+});
+const TileLayerNoSSR = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), {
+  ssr: false
+});
+const MarkerNoSSR = dynamic(() => import('react-leaflet').then(mod => mod.Marker), {
+  ssr: false
+});
+const PopupNoSSR = dynamic(() => import('react-leaflet').then(mod => mod.Popup), {
+  ssr: false
+});
+
+// Since L is not used server-side, it's safe to call it outside a useEffect or dynamic import
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const defaultIcon = L.icon({
   iconUrl: "/icons/MapIcon.svg",
@@ -16,13 +30,6 @@ const defaultIcon = L.icon({
   tooltipAnchor: [16, -28],
   shadowSize: [41, 41],
 });
-
-const MapContainerWithNoSSR = dynamic(
-  () => import("react-leaflet").then((mod) => mod.MapContainer),
-  {
-    ssr: false,
-  },
-);
 
 // Set the custom default icon as the default icon for all Leaflet markers
 L.Marker.prototype.options.icon = defaultIcon;
@@ -44,17 +51,17 @@ interface FarmerMapProps {
 
 const FarmerMap: React.FC<FarmerMapProps> = ({ farmers }) => {
   return (
-    <MapContainerWithNoSSR
+    <MapContainerNoSSR
       center={[51.505, -0.09]}
       zoom={3}
       minZoom={2}
       scrollWheelZoom={false}
       style={{ height: "40rem", width: "100%", borderRadius: "1rem" }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayerNoSSR url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {farmers.map((farmer) => (
-        <Marker key={farmer.id} position={[farmer.latitude, farmer.longitude]}>
-          <Popup>
+        <MarkerNoSSR key={farmer.id} position={[farmer.latitude, farmer.longitude]}>
+          <PopupNoSSR>
             <div className="flex flex-col p-0">
               <div className="relative mt-2 h-36 w-56 overflow-hidden rounded-md bg-gray-200">
                 <Image
@@ -73,17 +80,15 @@ const FarmerMap: React.FC<FarmerMapProps> = ({ farmers }) => {
                 </div>
                 <div>
                   <Link href={`/community/farmers/${farmer.slug}`}>
-                    <button className="rounded-md bg-darkgreen px-2 py-2 text-white hover:bg-deepgreen">
-                      See farm
-                    </button>
+                    <a className="rounded-md bg-darkgreen px-2 py-2 text-white hover:bg-deepgreen">See farm</a>
                   </Link>
                 </div>
               </div>
             </div>
-          </Popup>
-        </Marker>
+          </PopupNoSSR>
+        </MarkerNoSSR>
       ))}
-    </MapContainerWithNoSSR>
+    </MapContainerNoSSR>
   );
 };
 
