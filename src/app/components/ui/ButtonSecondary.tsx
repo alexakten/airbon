b2617eed1d21407aec256b9aade6632ef4091182
Alex <alex.akten@outlook.com>
2024-04-08 09:46:02 +0200

    go regen secondary button

--- src/app/components/ui/ButtonSecondary.tsx
+++ src/app/components/ui/ButtonSecondary.tsx
@@ -4,7 +4,7 @@ export default function ButtonSecondary() {
   return (
     <Link href={"mailto:alex.akten@outlook.com"}>
       <button className="group rounded-md bg-zinc-950 px-3 py-2 text-sm text-white hover:bg-blue">
-        Go regenerative <span className="inline-block tracking-normal translate-x-0 group-hover:translate-x-1 transition-transform duration-200">-&gt;</span>
+        Go regen <span className="inline-block tracking-normal translate-x-0 group-hover:translate-x-1 transition-transform duration-200">-&gt;</span>
       </button>
     </Link>
   )
