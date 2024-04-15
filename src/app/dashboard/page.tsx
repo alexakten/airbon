54589f8b2ebd6e64942885ba97bf5c57cf8f9869
Alex <alex.akten@outlook.com>
2024-04-15 09:20:35 +0200

    demo badge

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -49,6 +49,7 @@ export default function Dashboard() {
         {currentView === "learnFarmer" && <LearnFarmer />}
         {currentView === "communityFarmer" && <CommunityFarmer />}
       </div>
+      <div className="absolute z-[100] top-6 text-sm font-medium right-6 rounded-full bg-indigo-100 px-4 py-2 ">DEMO</div>
     </main>
   );
 }
